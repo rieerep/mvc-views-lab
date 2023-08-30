c5e4843109c6e3dc1082804bae7454eb0a261c35
rieerep <114030611+rieerep@users.noreply.github.com>
2023-08-30 09:44:38 +0200

    added files

---
+++ MVC-views-lab/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_views_lab.Models;
+using MVC_views_lab.Models.ViewModel;
+
+namespace MVC_views_lab.Controllers
+{
+    public class CustomerController : Controller
+    {
+        CustomerModel[] customers =
+            {
+                new CustomerModel { Id = 0, Name="Erkan", Email ="erk@me.com", Info = "Gold"},
+                new CustomerModel { Id = 1, Name="Bob", Email ="bob@live.se", Info = "Platinum"},
+                new CustomerModel { Id = 2, Name="Tove", Email="evot@mail.com", Info="Gold"}
+            };
+
+        //GET: customer/index
+        public IActionResult Index()
+        {
+
+            return View(customers);
+        }
+
+        //EDIT /Customer/Edit/1
+        
+        public IActionResult Edit(int id)
+        {
+
+            CustomerModel customer = customers[id];
+
+            //EditCustomerViewModel editPerson = new EditCustomerViewModel
+            //{
+            //    //Name = customer.Name,
+            //    Email = customer.Email,
+            //};
+
+            return View(customer);
+        }
+    }
+}
---
+++ MVC-views-lab/Models/ViewModel/EditCustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_views_lab.Models.ViewModel
+{
+    public class EditCustomerViewModel
+    {
+        [DisplayName("Name")]
+        [Required(ErrorMessage = "Please enter name")]
+        public string? Name { get; set; }
+        [DisplayName("Email")]
+        [Required(ErrorMessage = "Please enter Email")]
+        public string? Email { get; set; }
+    }
+}
